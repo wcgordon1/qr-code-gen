3b7d29d4295f906dfec162948a430d08946a6561
wcgordon1 <wcgordon1@gmail.com>
2024-09-18 08:32:21 -0700

    might mess this up, phone difficult, link

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,17 +3,17 @@ import Image from 'next/image';
 
 const Team = () => {
   const teamMembers = [
-    { name: "Will G.", role: "Janitor & Customer Support", image: "/images/me44.png" },
+    { name: "Will G.", role: "Janitor & Customer Service Rep", image: "/images/me44.png" },
     { name: "Larry L.", role: "Head of Technical Marketing", image: "/images/llama.webp" },
-    { name: "Arty I.", role: "Chief Technical Officer", image: "/images/artyi.jpg" },
+    { name: "Arty I.", role: "Chief Technical Officer", image: "/images/arti.webp" },
   ];
 
   return (
     <div className="bg-white py-6 sm:py-8 lg:py-12 mb-12">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="mb-10 md:mb-16">
-          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Meet our Team</h2>
-          <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
+          <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Meet Team Llama</h2>
+          <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">The brightest emerging talent in the QR code space came together to create the ultimate free QR code generator. Free for everyone. Free of ads. Free of emails.</p>
         </div>
 
         <div className="grid grid-cols-2 gap-x-4 gap-y-8 md:grid-cols-3 lg:gap-x-8 lg:gap-y-12">
@@ -24,7 +24,7 @@ const Team = () => {
               </div>
 
               <div>
-                <div className="font-bold text-indigo-500 md:text-lg">{member.name}</div>
+                <div className="font-bold text-indigo-600 md:text-lg">{member.name}</div>
                 <p className="mb-3 text-sm text-gray-500 md:mb-4 md:text-base">{member.role}</p>
 
                 
